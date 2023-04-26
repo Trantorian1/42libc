6bb014b10b3f93045ad9751b8c817d7dd7f352be
emcnab <eliot.code@proton.me>
2023-04-26 18:47:24 +0200

    fix(str): strcmp is now more consistent
    lib_strcmp now stops comparison at the first diverging character.
    Consistent behaviour is also guaranteed when one of the arguments is
    NULL (null is considered as 0 in that case).

--- src/str/lib_strcmp.c
+++ src/str/lib_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 09:50:17 by emcnab            #+#    #+#             */
-/*   Updated: 2023/04/26 09:55:55 by emcnab           ###   ########.fr       */
+/*   Updated: 2023/04/26 18:33:58 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include <stdlib.h>
 #include <errno.h>
+#include <stdio.h>
 
 int32_t	lib_strcmp(char *_Nonnull str_a, char *_Nonnull str_b)
 {
@@ -22,18 +23,21 @@ int32_t	lib_strcmp(char *_Nonnull str_a, char *_Nonnull str_b)
 	if (str_a == NULL || str_b == NULL)
 	{
 		errno = EINVAL;
+		if (str_b != NULL)
+			return (-*str_b);
+		if (str_a != NULL)
+			return (*str_a);
 		return (0);
 	}
 	diff = 0;
 	while (*str_a && *str_b)
 	{
 		diff += *str_a - *str_b;
+		if (*str_a != *str_b)
+			return (diff);
 		str_a++;
 		str_b++;
 	}
-	while (*str_a)
-		diff += *str_a++;
-	while (*str_b)
-		diff += *str_b++;
+	diff += *str_a - *str_b;
 	return (diff);
 }
