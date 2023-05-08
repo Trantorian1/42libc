0d2d63227de488b45f3ba8e843bc0cf4fe7edced
emcnab <eliot.code@proton.me>
2023-05-08 18:34:12 +0200

    docs(str): added doxygen to str_find

--- src/str/str_find.c
+++ src/str/str_find.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 18:10:47 by emcnab            #+#    #+#             */
-/*   Updated: 2023/05/08 18:15:26 by emcnab           ###   ########.fr       */
+/*   Updated: 2023/05/08 18:34:10 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,26 @@
 #include <stdlib.h>
 #include <errno.h>
 
-char *_Nonnull	str_find(char *_Nonnull str_src, char *_Nonnull to_find)
+/**
+ * @brief Searches for a set of tokens in a source string.
+ *
+ * @param str_src (char *_Nonnull): string to search.
+ * @param tokens (char *_Nonnull): characters to find in source string.
+ *
+ * @return (char *_Nonnull): first matching token in source string.
+ */
+char *_Nonnull	str_find(char *_Nonnull str_src, char *_Nonnull tokens)
 {
 	char	*cursor;
 
-	if (str_src == NULL || to_find == NULL)
+	if (str_src == NULL || tokens == NULL)
 	{
 		errno = EINVAL;
 		return (str_src);
 	}
 	while (*str_src != '\0')
 	{
-		cursor = to_find;
+		cursor = tokens;
 		while (*cursor != '\0')
 		{
 			if (*str_src == *cursor)
